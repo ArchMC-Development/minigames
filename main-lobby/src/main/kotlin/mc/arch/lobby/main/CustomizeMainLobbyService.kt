60b81fa4c1bb8e366d0f0444a0620584485d9daf
Subham K. <me@growly.gg>
2025-10-26 14:53:36 -0700

    feat: main lobby music & some wither updates

--- main-lobby/src/main/kotlin/mc/arch/lobby/main/CustomizeMainLobbyService.kt
+++ main-lobby/src/main/kotlin/mc/arch/lobby/main/CustomizeMainLobbyService.kt
@@ -3,7 +3,12 @@ package mc.arch.lobby.main
 import gg.scala.flavor.service.Configure
 import gg.scala.flavor.service.Service
 import gg.tropic.game.extensions.cosmetics.CosmeticLocalConfig
+import gg.tropic.game.extensions.music.Music
+import gg.tropic.game.extensions.music.Playlist
 import gg.tropic.practice.minigame.MinigameLobby
+import me.lucko.helper.Events
+import me.lucko.helper.Schedulers
+import org.bukkit.event.player.PlayerJoinEvent
 
 /**
  * @author Subham
@@ -15,7 +20,22 @@ object CustomizeMainLobbyService
     @Configure
     fun configure()
     {
-        CosmeticLocalConfig.enableCosmeticResources = false
         MinigameLobby.customizeMainLobby()
+
+        Events
+            .subscribe(PlayerJoinEvent::class.java)
+            .handler { event ->
+                Schedulers
+                    .sync()
+                    .runLater({
+                        Playlist(
+                            musicDiscs = listOf(
+                                Music.Strad,
+                                Music.Cat,
+                                Music.Wait,
+                            )
+                        ).play(event.player)
+                    }, 20L)
+            }
     }
 }
