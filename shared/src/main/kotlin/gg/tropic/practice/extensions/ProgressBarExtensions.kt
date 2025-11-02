40d98c2f6e06553ed2436b383aff680161915701
Subham K. <me@growly.gg>
2025-11-01 20:41:16 -0700

    feat: several updates

--- shared/src/main/kotlin/gg/tropic/practice/extensions/ProgressBarExtensions.kt
+++ shared/src/main/kotlin/gg/tropic/practice/extensions/ProgressBarExtensions.kt
@@ -1,6 +1,9 @@
 package gg.tropic.practice.extensions
 
 import net.evilblock.cubed.util.CC
+import net.evilblock.cubed.util.bukkit.Constants
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 /**
  * @author Subham
@@ -20,16 +23,33 @@ fun createProgressBar(progress: Double, total: Double): String
     return "$completedPart$remainingPart"
 }
 
+@OptIn(ExperimentalEncodingApi::class)
+val bar = "█"
+
 fun createProgressBarAlt(color: String, progress: Double, total: Double): String
 {
-    if (progress > total) return "$color${CC.STRIKE_THROUGH}█".repeat(18)
+    if (progress > total) return "$color${CC.STRIKE_THROUGH}$bar".repeat(18)
 
     val segments = 10
     val progressRatio = (progress / total).coerceIn(0.0, 1.0)
     val completedSegments = (segments * progressRatio).toInt()
 
-    val completedPart = "${color}${"█".repeat(completedSegments)}"
-    val remainingPart = "${CC.GRAY}${"█".repeat(segments - completedSegments)}"
+    val completedPart = "${color}${bar.repeat(completedSegments)}"
+    val remainingPart = "${CC.GRAY}${bar.repeat(segments - completedSegments)}"
+
+    return "$completedPart$remainingPart"
+}
+
+fun createProgressBarAlt2(color: String, progress: Double, total: Double): String
+{
+    if (progress > total) return "$color${CC.STRIKE_THROUGH}${Constants.THICK_VERTICAL_LINE}".repeat(18)
+
+    val segments = 10
+    val progressRatio = (progress / total).coerceIn(0.0, 1.0)
+    val completedSegments = (segments * progressRatio).toInt()
+
+    val completedPart = "${color}${"${Constants.THICK_VERTICAL_LINE}".repeat(completedSegments)}"
+    val remainingPart = "${CC.GRAY}${"${Constants.THICK_VERTICAL_LINE}".repeat(segments - completedSegments)}"
 
     return "$completedPart$remainingPart"
 }
