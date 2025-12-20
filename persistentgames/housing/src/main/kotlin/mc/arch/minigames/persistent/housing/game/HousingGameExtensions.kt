1029968869f37b62c322a6bd6031127c5e00b517
98ping <67809373+98ping@users.noreply.github.com>
2025-12-19 16:55:46 -0800

    feat: add way to visit houses now which is hype

--- persistentgames/housing/src/main/kotlin/mc/arch/minigames/persistent/housing/game/HousingGameExtensions.kt
+++ persistentgames/housing/src/main/kotlin/mc/arch/minigames/persistent/housing/game/HousingGameExtensions.kt
@@ -1,9 +1,20 @@
 package mc.arch.minigames.persistent.housing.game
 
+import gg.tropic.practice.persistence.RedisShared
+import gg.tropic.practice.ugc.HostedWorldRPC
+import gg.tropic.practice.ugc.PersistencePolicy
+import gg.tropic.practice.ugc.WorldInstanceProviderType
+import gg.tropic.practice.ugc.generation.visits.VisitWorldRequest
+import gg.tropic.practice.ugc.generation.visits.VisitWorldStatus
+import mc.arch.minigames.persistent.housing.api.VisitHouseConfiguration
 import mc.arch.minigames.persistent.housing.api.content.HousingItemStack
+import mc.arch.minigames.persistent.housing.api.model.PlayerHouse
+import net.evilblock.cubed.util.CC
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import java.util.concurrent.CompletableFuture
 
 /**
  * Class created on 12/18/2025
@@ -23,3 +34,45 @@ fun HousingItemStack.toBukkitStack(): ItemStack = ItemStack(Material.valueOf(thi
         itemStack.durability = this.data
         itemStack.itemMeta = itemMeta
     }
+
+fun PlayerHouse.processVisitation(player: Player): CompletableFuture<Void>
+{
+    player.sendMessage("${CC.GRAY}Traveling to this realm...")
+
+    return this.playerCanJoin(player.uniqueId).thenCompose {
+        if (!it)
+        {
+            player.sendMessage("${CC.RED}You are now allowed to join this house!")
+            return@thenCompose null
+        }
+
+        HostedWorldRPC.visitWorldRPCService
+            .call(
+                VisitWorldRequest(
+                    visitingPlayers = setOfNotNull(player.uniqueId),
+                    worldGlobalId = this.identifier,
+                    configuration = VisitHouseConfiguration(
+                        houseId = this.identifier,
+                        persistencePolicy = PersistencePolicy.PERSISTENT
+                    ),
+                    ownerPlayerId = player.uniqueId,
+                    providerType = WorldInstanceProviderType.REALM
+                )
+            )
+            .thenAccept { response ->
+                if (response.status == VisitWorldStatus.SUCCESS_REDIRECT)
+                {
+                    player.sendMessage("${CC.GREEN}You are being sent to this home!")
+                    RedisShared.redirect(listOf(player.uniqueId), response.redirectToInstance!!)
+                    return@thenAccept
+                }
+
+                player.sendMessage("${CC.RED}We weren't able to create a house for you. (${response.status})")
+            }
+            .exceptionally { throwable ->
+                throwable.printStackTrace()
+                player.sendMessage("${CC.RED}We weren't able to create a house for you right now. Try again later!")
+                return@exceptionally null
+            }
+    }
+}
