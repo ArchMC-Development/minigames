4c33c6d1c7934649c8bb177c85e3952857552c48
Subham K. <me@growly.gg>
2025-12-22 13:15:25 -0500

    feat: more private game systems

---
+++ game/src/main/kotlin/gg/tropic/practice/privategames/PrivateGamesHotbarService.kt
@@ -0,0 +1,112 @@
+package gg.tropic.practice.privategames
+
+import com.cryptomorin.xseries.XMaterial
+import gg.scala.flavor.inject.Inject
+import gg.scala.flavor.service.Configure
+import gg.scala.flavor.service.Service
+import gg.scala.lemon.redirection.impl.VelocityRedirectSystem
+import gg.tropic.practice.expectation.ExpectationService
+import gg.tropic.practice.games.GameState
+import gg.tropic.practice.games.event.PlayerJoinGameEvent
+import gg.tropic.practice.privategames.menu.PrivateGameSettingsMenu
+import me.lucko.helper.Events
+import net.evilblock.cubed.util.CC
+import net.evilblock.cubed.util.bukkit.ItemBuilder
+import org.bukkit.Material
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.plugin.java.JavaPlugin
+import java.time.Duration
+
+/**
+ * Service that adds hotbar items for private games in the waiting lobby.
+ * Allows the party leader to configure game settings before the game starts.
+ *
+ * @author GrowlyX
+ * @since 12/22/24
+ */
+@Service
+object PrivateGamesHotbarService
+{
+    @Inject
+    lateinit var plugin: JavaPlugin
+
+    private val settingsItem = ItemBuilder
+        .of(XMaterial.COMPARATOR)
+        .name("${CC.LIGHT_PURPLE}Game Settings ${CC.GRAY}(Right Click)")
+        .addToLore(
+            "${CC.GRAY}Configure private game",
+            "${CC.GRAY}settings before start!"
+        )
+        .build()
+
+    private val rateLimits = mutableMapOf<java.util.UUID, Long>()
+
+    @Configure
+    fun configure()
+    {
+        // Add settings item to private games on player join
+        Events
+            .subscribe(PlayerJoinGameEvent::class.java)
+            .filter { it.game.expectationModel.isPrivateGame }
+            .filter { it.game.state == GameState.Waiting || it.game.state == GameState.Starting }
+            .handler { event ->
+                // Add settings item to slot 2 for party leader
+                if (event.game.expectationModel.players.firstOrNull() == event.player.uniqueId)
+                {
+                    event.player.inventory.setItem(2, settingsItem)
+                    event.player.updateInventory()
+                    
+                    event.player.sendMessage("${CC.LIGHT_PURPLE}This is a Private Game! Right-click the Comparator to configure settings.")
+                }
+            }
+            .bindWith(plugin)
+
+        // Handle settings item click
+        Events
+            .subscribe(PlayerInteractEvent::class.java)
+            .filter { 
+                it.hasItem() && 
+                it.item.isSimilar(settingsItem) &&
+                (it.action == Action.RIGHT_CLICK_AIR || it.action == Action.RIGHT_CLICK_BLOCK)
+            }
+            .handler { event ->
+                val game = gg.tropic.practice.games.GameService.byPlayer(event.player)
+                    ?: return@handler
+
+                if (!game.expectationModel.isPrivateGame)
+                {
+                    return@handler
+                }
+
+                // Rate limit check
+                val lastClick = rateLimits[event.player.uniqueId] ?: 0L
+                if (System.currentTimeMillis() - lastClick < 250L)
+                {
+                    return@handler
+                }
+                rateLimits[event.player.uniqueId] = System.currentTimeMillis()
+
+                // Check if player is party leader (first in players list)
+                if (game.expectationModel.players.firstOrNull() != event.player.uniqueId)
+                {
+                    event.player.sendMessage("${CC.RED}Only the party leader can modify game settings!")
+                    return@handler
+                }
+
+                if (!(game.state == GameState.Waiting || game.state == GameState.Starting))
+                {
+                    event.player.sendMessage("${CC.RED}You can only modify settings before the game starts!")
+                    return@handler
+                }
+
+                // Get game type from minigame lifecycle
+                val gameType = game.miniGameLifecycle?.let {
+                    game.flagMetaData(gg.tropic.practice.kit.feature.FeatureFlag.MiniGameType, "id")
+                } ?: "default"
+
+                PrivateGameSettingsMenu(game, gameType).openMenu(event.player)
+            }
+            .bindWith(plugin)
+    }
+}
---
+++ game/src/main/kotlin/gg/tropic/practice/privategames/menu/PrivateGameSettingsMenu.kt
@@ -0,0 +1,204 @@
+package gg.tropic.practice.privategames.menu
+
+import com.cryptomorin.xseries.XMaterial
+import gg.tropic.practice.games.GameImpl
+import gg.tropic.practice.games.GameState
+import gg.tropic.practice.privategames.settings.PrivateGameSetting
+import gg.tropic.practice.privategames.settings.PrivateGameSettingsRegistry
+import gg.tropic.practice.privategames.settings.impl.BooleanSetting
+import gg.tropic.practice.privategames.settings.impl.DoubleSetting
+import gg.tropic.practice.privategames.settings.impl.IntSetting
+import net.evilblock.cubed.menu.Button
+import net.evilblock.cubed.menu.pagination.PaginatedMenu
+import net.evilblock.cubed.util.CC
+import net.evilblock.cubed.util.bukkit.ItemBuilder
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.ClickType
+import org.bukkit.inventory.InventoryView
+
+/**
+ * Dynamic menu for editing private game settings in the waiting lobby.
+ * Automatically renders settings based on the game type.
+ *
+ * @author GrowlyX
+ * @since 12/22/24
+ */
+class PrivateGameSettingsMenu(
+    private val game: GameImpl,
+    private val gameType: String
+) : PaginatedMenu()
+{
+    // Load settings for this game type
+    private val settings: MutableList<PrivateGameSetting<*>> = mutableListOf()
+
+    init
+    {
+        updateAfterClick = true
+
+        // Load settings from registry if not already stored in game
+        val existingSettings = game.expectationModel.privateGameSettings?.gameSpecificSettings
+        val freshSettings = PrivateGameSettingsRegistry.getSettingsFor(gameType)
+
+        freshSettings.forEach { setting ->
+            // Restore any previously saved values
+            existingSettings?.get(setting.id)?.let { savedValue ->
+                @Suppress("UNCHECKED_CAST")
+                when (setting) {
+                    is BooleanSetting -> setting.value = savedValue as? Boolean ?: setting.defaultValue
+                    is IntSetting -> setting.value = savedValue as? Int ?: setting.defaultValue
+                    is DoubleSetting -> setting.value = savedValue as? Double ?: setting.defaultValue
+                }
+            }
+            settings.add(setting)
+        }
+    }
+
+    override fun getPrePaginatedTitle(player: Player) = "Private Game Settings"
+
+    override fun getAllPagesButtons(player: Player): Map<Int, Button>
+    {
+        val buttons = mutableMapOf<Int, Button>()
+
+        settings.forEachIndexed { index, setting ->
+            buttons[index] = SettingButton(setting)
+        }
+
+        return buttons
+    }
+
+    override fun getMaxItemsPerPage(player: Player) = 21
+
+    override fun onClose(player: Player, manualClose: Boolean)
+    {
+        // Save settings back to game expectation
+        val privateSettings = game.expectationModel.privateGameSettings ?: return
+        settings.forEach { setting ->
+            privateSettings.gameSpecificSettings[setting.id] = setting.value as Any
+        }
+    }
+
+    private inner class SettingButton(
+        private val setting: PrivateGameSetting<*>
+    ) : Button()
+    {
+        override fun getName(player: Player): String
+        {
+            return "${CC.GREEN}${setting.displayName}"
+        }
+
+        override fun getDescription(player: Player): List<String>
+        {
+            val lines = mutableListOf<String>()
+
+            setting.description.forEach {
+                lines.add("${CC.GRAY}$it")
+            }
+
+            lines.add("")
+            lines.add("${CC.WHITE}Current: ${CC.AQUA}${formatValue(setting.value)}")
+            lines.add("")
+
+            when (setting) {
+                is BooleanSetting -> {
+                    lines.add("${CC.YELLOW}Click to toggle!")
+                }
+                is IntSetting, is DoubleSetting -> {
+                    lines.add("${CC.YELLOW}Left-Click to increase")
+                    lines.add("${CC.YELLOW}Right-Click to decrease")
+                    lines.add("${CC.YELLOW}Middle-Click to reset")
+                }
+                else -> {
+                    lines.add("${CC.YELLOW}Click to cycle!")
+                }
+            }
+
+            return lines
+        }
+
+        override fun getMaterial(player: Player): XMaterial
+        {
+            return when (setting) {
+                is BooleanSetting -> if (setting.value) XMaterial.LIME_DYE else XMaterial.GRAY_DYE
+                is IntSetting -> XMaterial.CLOCK
+                is DoubleSetting -> XMaterial.GOLD_NUGGET
+                else -> XMaterial.PAPER
+            }
+        }
+
+        override fun clicked(player: Player, slot: Int, clickType: ClickType, view: InventoryView)
+        {
+            if (!(game.state == GameState.Waiting || game.state == GameState.Starting))
+            {
+                player.sendMessage("${CC.RED}You can only modify settings before the game starts!")
+                return
+            }
+
+            when (setting) {
+                is BooleanSetting -> {
+                    setting.cycleValue()
+                    player.sendMessage("${CC.GREEN}${setting.displayName} ${CC.GRAY}set to ${CC.AQUA}${formatValue(setting.value)}")
+                }
+                is IntSetting -> {
+                    when (clickType) {
+                        ClickType.LEFT -> {
+                            val options = setting.availableValues()
+                            val currentIndex = options.indexOf(setting.value)
+                            if (currentIndex < options.size - 1) {
+                                setting.value = options[currentIndex + 1]
+                            }
+                        }
+                        ClickType.RIGHT -> {
+                            val options = setting.availableValues()
+                            val currentIndex = options.indexOf(setting.value)
+                            if (currentIndex > 0) {
+                                setting.value = options[currentIndex - 1]
+                            }
+                        }
+                        ClickType.MIDDLE -> setting.reset()
+                        else -> {}
+                    }
+                    player.sendMessage("${CC.GREEN}${setting.displayName} ${CC.GRAY}set to ${CC.AQUA}${formatValue(setting.value)}")
+                }
+                is DoubleSetting -> {
+                    when (clickType) {
+                        ClickType.LEFT -> {
+                            val options = setting.availableValues()
+                            val currentIndex = options.indexOf(setting.value)
+                            if (currentIndex < options.size - 1) {
+                                setting.value = options[currentIndex + 1]
+                            }
+                        }
+                        ClickType.RIGHT -> {
+                            val options = setting.availableValues()
+                            val currentIndex = options.indexOf(setting.value)
+                            if (currentIndex > 0) {
+                                setting.value = options[currentIndex - 1]
+                            }
+                        }
+                        ClickType.MIDDLE -> setting.reset()
+                        else -> {}
+                    }
+                    player.sendMessage("${CC.GREEN}${setting.displayName} ${CC.GRAY}set to ${CC.AQUA}${formatValue(setting.value)}")
+                }
+                else -> {
+                    setting.cycleValue()
+                    player.sendMessage("${CC.GREEN}${setting.displayName} ${CC.GRAY}set to ${CC.AQUA}${formatValue(setting.value)}")
+                }
+            }
+
+            // Save to game settings immediately
+            val privateSettings = game.expectationModel.privateGameSettings ?: return
+            privateSettings.gameSpecificSettings[setting.id] = setting.value as Any
+        }
+
+        private fun formatValue(value: Any?): String
+        {
+            return when (value) {
+                is Boolean -> if (value) "Enabled" else "Disabled"
+                is Double -> "%.2f".format(value)
+                else -> value?.toString() ?: "N/A"
+            }
+        }
+    }
+}
